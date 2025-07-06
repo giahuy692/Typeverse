1737784c0a760cf186e832e0e868aad064e5aae1
giahuy692 <yuhaigod8485@gmail.com>
2025-07-06 14:26:18 +0700

    thêm no-image

--- src/app/shared/components/writing/writing.component.ts
+++ src/app/shared/components/writing/writing.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { speakinglist } from 'src/assets/mock-data/speaking-practise';
 import { WRITING_PROMPTS, WritingPrompt } from 'src/assets/mock-data/writing-prompts';
 import { DTOListQuestion } from '../../DTO/DTOListQuestion';
+import { LIST_QUESTIONS } from 'src/assets/mock-data/list-question-data';
 // Import mảng template. Đổi đường dẫn cho đúng project bạn!
 
 @Component({
@@ -10,5 +11,5 @@ import { DTOListQuestion } from '../../DTO/DTOListQuestion';
   styleUrls: ['./writing.component.scss'],
 })
 export class WritingComponent {
-  data: DTOListQuestion[] = speakinglist;
+  data: DTOListQuestion[] = LIST_QUESTIONS;
 }
